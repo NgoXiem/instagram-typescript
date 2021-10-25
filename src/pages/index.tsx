0153450446439f436f80bc948bb5f1b8f13f48f8
Xiem Ngo <xiemnt05.ssc@gmail.com>
2021-10-25 10:08:21 +0700

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div className="text-red-500">
+     Hello, I'm instagram again, but not the same! 
+    </div>
+  )
+}
+
+export default Home
